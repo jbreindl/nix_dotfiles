43c14f74429737748a70f4931943394076b1dadf
Julius Breindl <jbr@condat.de>
2026-05-18 08:34:03 +0200

    Wezterm config

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }:
 {
 
+  programs.wezterm = {
+    enable = true;
+  };
+
+  home.file = {
+    ".config/wezterm/wezterm.lua".source      = ./wezterm/wezterm.lua;
+    ".config/wezterm/appearance.lua".source   = ./wezterm/appearance.lua;
+    ".config/wezterm/multiplexing.lua".source = ./wezterm/multiplexing.lua;
+    ".config/wezterm/tabline.lua".source      = ./wezterm/tabline.lua;
+  };
+
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1"; # For Electron/Chromium-based apps
     MOZ_ENABLE_WAYLAND = "1"; # For Firefox
@@ -21,7 +32,6 @@
       '';
     })
     firefox
-    wezterm
     fira-code
     nerd-fonts.fira-code
     networkmanager
