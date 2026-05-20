ed2626a1ad1422c92bbce665d8e9030042a14f34
Julius Breindl <jbr@condat.de>
2026-05-20 22:03:56 +0200

    Add helix to base config

--- modules/cli.nix
+++ modules/cli.nix
@@ -34,8 +34,9 @@
     clippy
     rustc
     rustfmt
+    helix
   ];
-  imports = [./zsh.nix];
+  imports = [ ./zsh.nix ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
