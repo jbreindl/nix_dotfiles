f6d24b400f12f0421f220aa41edeaaa1cf3c1c12
Julius Breindl <jbr@condat.de>
2026-05-20 21:03:09 +0200

    Add colemak dh to base layer

--- modules/sway/sway-work.nix
+++ modules/sway/sway-work.nix
@@ -2,8 +2,8 @@
 
 {
   imports = [
-        ./base.nix
-    ];
+    ./base.nix
+  ];
   wayland.windowManager.sway = {
     config = {
       assigns = {
@@ -17,8 +17,13 @@
         { command = "teams-for-linux"; }
         { command = "outlook-for-linux"; }
       ];
+      input = {
+        "1:1:AT_Translated_Set_2_keyboard" = {
+          xkb_layout = "us";
+          xkb_variant = "colemak_dh_iso";
+        };
+      };
+
     };
-
-
   };
 }
