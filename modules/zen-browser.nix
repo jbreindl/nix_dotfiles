4bc9edad3e6824ec83edde6232acda2ed7741bbb
Julius Breindl <jbr@condat.de>
2026-06-14 10:45:19 +0200

    Updates for Zen

--- modules/zen-browser.nix
+++ modules/zen-browser.nix
@@ -1,4 +1,32 @@
 { ... }:
 {
   programs.zen-browser.enable = true;
+  programs.zen-browser.profiles = {
+    default = {
+      pins = {
+        "Email" = {
+          id = "4d246ec1-0909-4f04-b907-431fcbaadabe";
+          url = "https://gmail.com";
+          position = 101;
+          isEssential = true;
+        };
+      };
+      spaces = {
+        "Test" = {
+          id = "space:test";
+          position = 1000;
+        };
+      };
+      settings = {
+
+        "zen.view.compact.hide-tabbar" = true;
+      };
+
+    };
+  };
+
+  stylix.targets.zen-browser.profileNames = [
+    "default"
+  ];
+  stylix.targets.zen-browser.enable = true;
 }
