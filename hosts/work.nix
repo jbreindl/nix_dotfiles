74ba7506efc6591d0c04afecd88e9cda27e4d279
Julius Breindl <jbr@condat.de>
2026-08-09 12:13:22 +0200

    Remove commented code, manage git via home manager

--- hosts/work.nix
+++ hosts/work.nix
@@ -1,18 +1,29 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.username = "jbr";
   home.homeDirectory = "/home/jbr";
   imports = [
     ../modules/cli.nix
     ../modules/desktop.nix
-    # ../modules/sway/sway-work.nix
     ../modules/niri/base.nix
-    # ../modules/hyprland/base.nix
   ];
 
   targets = {
     # Make home-manager work better on non-NixOS
     genericLinux.enable = true;
     genericLinux.gpu.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    thunderbird
+    teams-for-linux
+  ];
+
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "Julius Breindl";
+      email = "jbr@condat.de";
+    };
   };
 }
