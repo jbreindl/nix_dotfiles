afede6843ec56324fad5b06d14bf3e10f2ddbc6d
Julius Breindl <jbr@condat.de>
2026-08-30 10:18:59 +0200

    Fix typo

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -14,7 +14,7 @@ in
 
     fonts = {
       monospace = {
-        package = pkgs.nerd-font.fira-code;
+        package = pkgs.nerd-fonts.fira-code;
         name = "FiraCode Nerd Font";
       };
       sizes.terminal = 16;
