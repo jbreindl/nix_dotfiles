58602fd55d432d171674a3c5aff788ffd0b2facc
Julius Breindl <juliusb1616@gmail.com>
2026-09-15 16:35:31 +0200

    New keybinds

--- modules/nixvim/keys.nix
+++ modules/nixvim/keys.nix
@@ -1,8 +1,24 @@
 {
   keymaps = [
     {
-      action = "+y";
+      action = "\"+y";
       key = "<leader>y";
+      mode = [
+        "n"
+        "v"
+      ];
+    }
+    {
+      action = "\"+p";
+      key = "<leader>p";
+      mode = [
+        "n"
+        "v"
+      ];
+    }
+    {
+      action = "\"+P";
+      key = "<leader>P";
       mode = [
         "n"
         "v"
